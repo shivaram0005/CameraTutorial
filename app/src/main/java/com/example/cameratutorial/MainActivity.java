bfc1b92359f7fabff86e4dca5b286821fe38383d
shivaram0005 <shivaram8534@gmail.com>
2020-12-08 15:39:51 +0530

    CameraTutorial in Android

---
+++ app/src/main/java/com/example/cameratutorial/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.cameratutorial;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.iv);
+        b = (Button) findViewById(R.id.b1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+
+
+            Bitmap photo = (Bitmap) data.getExtras()
+                    .get("data");
+            iv.setImageBitmap(photo);
+        }
+    }
+}
